f64832ee69d70edc09debab36c87752de5a711b9
Lucia <luciferma14@gmail.com>
2024-06-10 19:18:55 +0200

    [] - Implementación de funciones

--- tests/test_mult.py
+++ tests/test_mult.py
@@ -10,6 +10,6 @@ class TestClass:
     # Test para la operación mult
     def test_mult(self):
         assert mult(4,5) == 20
-        assert mult(5,-5) == 25
+        assert mult(5,-5) == -25
         assert mult(7,5) == 35
         assert mult(10,9) == 90
